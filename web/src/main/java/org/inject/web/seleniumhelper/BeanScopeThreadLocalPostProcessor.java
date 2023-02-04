51e08d3532d31cbb8cf19b176885e401f7b373f4
RUSLAN_ZABABURIN <ruslan.zababurin@simbirsoft.com>
2023-02-04 18:19:25 +0300

    postprocessor

--- web/src/main/java/org/inject/web/seleniumhelper/BeanScopeThreadLocalPostProcessor.java
+++ web/src/main/java/org/inject/web/seleniumhelper/BeanScopeThreadLocalPostProcessor.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.SimpleThreadScope;
 import org.springframework.stereotype.Component;
 
 @Component
-public class BeanPP implements BeanFactoryPostProcessor {
+public class BeanScopeThreadLocalPostProcessor implements BeanFactoryPostProcessor {
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
         beanFactory.registerScope("threadlocal", new SimpleThreadScope());
